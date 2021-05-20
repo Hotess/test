85536b1ff68e9987bb3f8c6889d46716cac49bac
Pavel <hote_95@bk.ru>
2021-05-20 21:53:23 +0300

    fix: all

--- src/components/App.js
+++ src/components/App.js
@@ -1,145 +1,91 @@
 import React, { useState } from "react";
 
 function App() {
-  const [ model, setModel ] = useState([]);
-  const [ selectedModel, setSelectedModel ] = useState([]);
-  const [ colorsModel, setColorsModel ] = useState({});
-  const dataModels = {
-    name: model.name,
-    paramValues: [
-      {
-        paramId: 1,
-        value: model.purpose
-      },
-      {
-        paramId: 2,
-        value:  model.width
-      }
-    ],
-    colors: [colorsModel].map((item) => (
+    const [model, setModel] = useState({});
+    const [models, setModels] = useState([]);
+    const [showModel, setShowModel] = useState(false);
 
-        {
-          name: item.color,
-          paramValues: [
-              {
-                paramId: 3,
-                value: item.complementaryСolors
-              }
-          ],
-          sizes: [item.RussianSize].map((size) => (
-              {
-                name: size,
-                paramValues: [
-                  {
-                    paramId: 4,
-                    value: size
-                  }
-                ]
-              })
-          )
-        })
-    )
-  }
+    /** Собрать данные из каждого поля ввода для редактора */
+    function onChangeModel(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
 
-  /** Получить данные с полей  */
-  function onChangeModel(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    (target.options !== undefined)
-        ? setSelectedModel(Array.from(target.options).filter(option => option.selected).map(option => option.value))
-        : setModel({...model, [name]: value});
-
-    if (name === 'color' || name === 'complementaryСolors' || name === 'RussianSize') {
-      setColorsModel({...colorsModel, [name]: value, RussianSize: selectedModel })
+        setModel({...model,  [name]: value });
     }
-  }
 
-  /** Добавить форму цвета и размеров */
-  function handleClickColors(event) {
-    event.target.after(addColors());
-  }
+    /** Показать редактор */
+    function getModel(event) {
+        event.preventDefault();
 
-  /** Получить структуру данных */
-  function getModel() {
-    const dataModelsJSON = JSON.stringify(dataModels);
+        setModels([...models,  model ]);
 
-    console.log(dataModelsJSON);
-  }
+        setShowModel(true);
+    }
 
-  function addColors() {
-    return (
-        <>
-          <label className='form__label'>Цвет
-            <input
-                type='text'
-                name='color'
-                minLength='6'
-                required='required'
-                onChange={onChangeModel}/>
-          </label>
-          <label className='form__label'>Дополнительные цвета
-            <input
-                type='text'
-                name='complementaryСolors'
-                minLength='6'
-                required='required'
-                onChange={onChangeModel}/>
-          </label>
-          <div>Российский размер</div>
-          <select id='select' name='RussianSize' className='form__select' required
-                  multiple size='8' onInput={onChangeModel}>
-            <option>40 S</option>
-            <option>42 S</option>
-            <option>44 M</option>
-            <option>46 M</option>
-            <option>48 L</option>
-            <option>50 L</option>
-            <option>52 XXL</option>
-            <option>54 XXL</option>
-          </select>
-        </>
-    );
-  }
+    /** Удалить поле ввода в редакторе */
+    function deleteInput(index) {
+        setModels(models.filter((item, i, array) => index !== i));
+    }
 
   return (
     <div className='page'>
       <section className='manipulation-model'>
-        <form className='form'>
-          <label className='form__label'>Название
+        <form className='form' onSubmit={getModel}>
+          <label className='form__label'>Название параметра
             <input
                 type='text'
-                name='name'
+                name='paramName'
+                minLength='1'
                 required='required'
                 onChange={onChangeModel}/>
           </label>
-          <div className='form__container'>
-            <label>
-              <input type='radio' name='purpose' value='daily' className='form__radio' onChange={onChangeModel}/>
-              Повседневный</label>
-            <label>
-              <input type='radio' name='purpose' value='festive' className='form__radio' onChange={onChangeModel}/>
-              Праздничный</label>
-          </div>
-          <label className='form__label'>Длина
-            <input
-                type='text'
-                name='width'
-                minLength='2'
-                required='required'
-                onChange={onChangeModel}/>
-          </label>
-          {addColors()}
+            <label className='form__label'>Уровень параметра
+                <input
+                    type='text'
+                    name='paramLevel'
+                    minLength='1'
+                    required='required'
+                    onChange={onChangeModel}
+                />
+            </label>
+            <label className='form__label'>Id параметра
+                <input
+                    type='text'
+                    name='paramId'
+                    minLength='1'
+                    required='required'
+                    onChange={onChangeModel}
+                />
+            </label>
           <button
-              type='button'
-              onClick={handleClickColors}
-          >Добавить цвет</button>
-          <button
-              type='button'
-              onClick={getModel}
-          >Показать структуру</button>
+              type='submit'
+              className='form__open'
+          >Показать редактор</button>
         </form>
+          { showModel ? (
+              <form className='form'>
+                  <h2 className='form__title'>Редактор</h2>
+                  <label className='form__label'>Название товара
+                      <input
+                          type='text'
+                          required='required'/>
+                  </label>
+                  {models.length !== 0 ? models.map((item, index) => (
+                      <div className='form__container' key={index}>
+                          <label className='form__label'>{item.paramName}
+                              <input type='text'/>
+                          </label>
+                          <button
+                              type='button'
+                              className='form__close'
+                              onClick={() => deleteInput(index)}
+                          />
+                      </div>
+                      )
+                  ) : null}
+              </form>
+          ): null }
       </section>
     </div>
   );
